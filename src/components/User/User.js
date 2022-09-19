3cb0e114151cad4c90014ad497302450c4d5e3ee
AndrewStets <astetskiv1@gmail.com>
2022-09-19 17:01:34 +0300

    33

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 const User = (props) => {
-
     const {item:user} = props;
 
     return (
         <div>
-            <h3>id : {user.id}</h3>
-            <h3>name : {user.name}</h3>
-            <h3>username : {user.username}</h3>
+            <h3>Id : {user.id}</h3>
+            <h3>Name : {user.name}</h3>
+            <h3>Username : {user.username}</h3>
             <button onClick={()=>{
 
             }}>Posts</button>
